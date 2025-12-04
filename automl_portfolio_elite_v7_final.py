b9461f182f6024a6e9f8cfbed85e5e6012380d35
fmaiaaa <153874352+fmaiaaa@users.noreply.github.com>
2025-12-04 11:49:39 -0300

    Update automl_portfolio_elite_v7_final.py

--- automl_portfolio_elite_v7_final.py
+++ automl_portfolio_elite_v7_final.py
@@ -171,6 +171,7 @@ ATIVOS_POR_SETOR_IBOV = {
     'Bens Industriais': ['EMBR3.SA', 'VAMO3.SA', 'WEGE3.SA', 'VIVA3.SA', 'ASAI3.SA', 'SMFT3.SA', 'CMIN3.SA', 'SLCE3.SA'],
     'Consumo Cíclico': ['AZZA3.SA', 'ALOS3.SA', 'CEAB3.SA', 'COGN3.SA', 'CURY3.SA', 'CVCB3.SA', 'CYRE3.SA', 'DIRR3.SA', 'LREN3.SA', 'MGLU3.SA', 'MRVE3.SA', 'MULT3.SA', 'NATU3.SA', 'PCAR3.SA', 'RENT3.SA', 'YDUQ3.SA'],
     'Consumo não Cíclico': ['BEEF3.SA', 'NATU3.SA', 'PCAR3.SA', 'VIVA3.SA'],
+    'Financeiro': ['B3SA3.SA', 'BBSE3.SA'],
     'Materiais Básicos': ['BRAP4.SA', 'BRKM5.SA', 'CSNA3.SA', 'GGBR4.SA', 'GOAU4.SA', 'KLBN11.SA', 'POMO4.SA', 'SUZB3.SA', 'USIM5.SA', 'VALE3.SA'],
     'Petróleo, Gás e Biocombustíveis': ['ENEV3.SA', 'PETR3.SA', 'PETR4.SA', 'PRIO3.SA', 'RAIZ4.SA', 'RECV3.SA', 'UGPA3.SA', 'VBBR3.SA'],
     'Saúde': ['FLRY3.SA', 'HAPV3.SA', 'RADL3.SA'],
